3e7e621a33fe33a826c85807696fbd393fbe7bd7
Víctor Caro Pastor <victorcaro199@gmail.com>
2024-08-28 22:00:00 +0100

    Update servos.ino

--- Code/servos/servos.ino
+++ Code/servos/servos.ino
@@ -1,8 +1,10 @@
-/*
-Programa de los servos de nuestro mayor proyecto: una galeria de tiro automatizada controlada por arduino y android
-Realizado por Víctor Caro Pastor y Alba Navarro Moreno, estudiantes de 2º Bachicherato  en el IES ZOCO
+/** Encabezado:
+ * @file servos.ino
+ * @brief Programa de los servos de la galeria de tiro automatizada controlada por arduino y android
+ * @author [Victor Caro Pastor](https://github.com/Vicaro1)
+ * @version  V2.1
+ * @date  07-07-2016
 */
-
 #include <Servo.h> // libreria para los servos 
 
 int angulo1=0; // variable para guardar el valor del angulo1 del servo
@@ -14,23 +16,31 @@ int x=0;       // variable para guardar los grados de los servos
 Servo servo1; // apodo para el servo 1
 Servo servo2; // apodo para el servo 2
 
-void setup() // esto lo hara al comienzo de cada "partida" solo una vez
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ SETUP ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+// Esto lo hara al comienzo de cada "partida" solo una vez
+void setup() 
 {
  Serial.begin(9600); // iniciamos la comunicacion serial para el bluetooth y el movil
  servo1.attach(9);   // indicamos el pin PWM del servo 1
  servo2.attach(10);  // indicamos el pin PWM del servo 2
 }
 
-void loop() // lo que repetira una y otra vez
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ LOOP ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+// lo que repetira una y otra vez
+void loop() 
 {
 
   if(Serial.available()>0) // nos aseguramos que el puerto serial esta habilitado y tiene una variable minimo
-{
+  {
   estado=Serial.read(); // la valor de la variable estado sera la lectura serial  
   }
   
  if(estado=='1') // si estado igual a 1 (encender) entonces...
-{
+ {
    if (lado ==0)
    {
     for (x=0; x<180; x+=4) // para un angulo igual a 40º y menor a 180º, suma 5º
@@ -61,15 +71,15 @@ void loop() // lo que repetira una y otra vez
    }
 }
  if(estado=='2') // si estado igual a 2 (apagar) entonces...
-     {  
-      angulo1 = 98;
-      angulo2 = 100;
-      servo1.write(angulo1);  // posicionamos el servo1 
-      servo2.write(angulo2);  // posicionamos el servo2     
-               
-      delay(100);  
-     }
-  }
+ {  
+  angulo1 = 98;
+  angulo2 = 100;
+  servo1.write(angulo1);  // posicionamos el servo1 
+  servo2.write(angulo2);  // posicionamos el servo2     
+           
+  delay(100);  
+ }
+}
 
   
   
